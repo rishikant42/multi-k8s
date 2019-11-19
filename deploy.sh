7e030baa6735c585bdb9ed9e18b9e603342b5bc1
rishikant42 <rshkntshrm@gmail.com>
2019-11-19 15:35:04 +0530

    assigne version

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
-docker build -t rishikant42/multi-client -f ./client/Dockerfile ./client
-docker build -t rishikant42/multi-server -f ./server/Dockerfile  ./server
-docker build -t rishikant42/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t rishikant42/multi-client:latest -t rishikant42/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rishikant42/multi-server:latest -t rishikant42/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rishikant42/multi-worker:latest -t rishikant42/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push rishikant42/multi-client
-docker push rishikant42/multi-server
-docker push rishikant42/multi-worker
+docker push rishikant42/multi-client:latest
+docker push rishikant42/multi-server:latest
+docker push rishikant42/multi-worker:latest
+
+docker push rishikant42/multi-client:$SHA
+docker push rishikant42/multi-server:$SHA
+docker push rishikant42/multi-worker:$SHA
 
 kubectl apply -f k8s
+kubectl set image deployment/client-deployment client=rishikant42/multi-client:$SHA
+kubectl set image deployment/server-deployment server=rishikant42/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=rishikant42/multi-worker:$SHA
